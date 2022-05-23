db858e27a94ef5e40508ccd112b27351243ac340
christopherfulldev <highlander.all@gmail.com>
2022-05-23 15:29:09 -0300

    fix: removido logger do projeto

--- server/error/index.js
+++ server/error/index.js
@@ -1,20 +1,11 @@
-'use strict'
-
-const logger = require("../server/util")
-
+'use strict';
 const errorHandler = {
     handler: function(error, response) {
-        console.log(error);
         if(error.message.includes('ENOENT')) {
-            logger.warn(`Asset not found ${error.stack}`);
             response.writeHead(404);
-
             return response.end();
-        }
-        
-        logger.error(`Caugth error on API ${error.stack}`);
+        };
         response.writeHead(500);
-        
         return response.end();
     }
 };
